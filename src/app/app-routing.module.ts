8a78a6986c14923eefcc659cb3c200ef6b117fec
denzil <denzil@MacBook-Pro-denzil.local>
2020-04-26 14:54:39 +0300

    initial for home, main, post pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainLayoutComponent} from './components/main-layout/main-layout.component';
+import {HomePageComponent} from './components/home-page/home-page.component';
+import {PostPageComponent} from './components/post-page/post-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: MainLayoutComponent, children: [
+        {path: '', redirectTo: '/', pathMatch: 'full'},
+        {path: '', component: HomePageComponent},
+        {path: 'post/:id', component: PostPageComponent}
+    ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { PostPageComponent } from './components/post-page/post-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HomePageComponent,
+    PostPageComponent
   ],
   imports: [
     BrowserModule,
