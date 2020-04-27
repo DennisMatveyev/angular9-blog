28c98c41ebeeb3b2bd0bd7c5d449e1961a4c04e2
denzil <denzil@MacBook-Pro-denzil.local>
2020-04-27 18:05:30 +0300

    admin layout

--- src/app/admin/components/admin-layout/admin-layout.component.ts
+++ src/app/admin/components/admin-layout/admin-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-admin-layout',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-layout.component.scss']
 })
 export class AdminLayoutComponent implements OnInit {
+    constructor(private router: Router) { }
 
-  constructor() { }
+    ngOnInit(): void { }
 
-  ngOnInit(): void {
-  }
+    logout(event: Event) {
+        event.preventDefault();
+        this.router.navigate(['/admin', 'login']);
+    }
 
 }
