3f030de68a146e7c7f2651365c6cbaefd2e2f9ce
denzil <denzil@MacBook-Pro-denzil.local>
2020-05-01 18:35:04 +0300

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
     production: true,
-    apiKey: 'AIzaSyDFsv6GaljM8MQUf-UakRz4wUhLviZN4ys',
-    firebaseDbUrl: 'https://angular-blog-9ac41.firebaseio.com'
+    apiKey: '',
+    firebaseDbUrl: ''
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
     production: false,
-    apiKey: 'AIzaSyDFsv6GaljM8MQUf-UakRz4wUhLviZN4ys',
-    firebaseDbUrl: 'https://angular-blog-9ac41.firebaseio.com'
+    apiKey: '',
+    firebaseDbUrl: ''
 };
 
 /*
